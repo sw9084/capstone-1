da1c52a98ba5a53c226d68f3a32994345e3f9c58
Bethlehem <aberabetty3@gmail.com>
2025-10-15 21:53:55 -0400

    Add Transaction class with date, time, description, vendor, and amount fields
    Added Transaction.java with documentation and formatting improvements
    
    fixed typo errors
    Added ledger class - load all transaction from transaction.csv
    added displays only deposit transaction and payment transaction

--- src/main/java/com/pluralsight/Ledger.java
+++ src/main/java/com/pluralsight/Ledger.java
@@ -12,8 +12,10 @@ import java.io.*;
 
 public class Ledger {
     private static final String FILE_PATH = "transactions.csv";
+
     /**
      * loads all transaction from the csv file
+     *
      * @return a list of transaction objects.
      */
     public static List<Transaction> loadTransactions() {
@@ -25,9 +27,70 @@ public class Ledger {
                 transactions.add(Transaction.fromCSVLine(line));
             }
         } catch (IOException e) {
-            System.out.println("Error loading transactions: "+ e.getMessage());
+            System.out.println("Error loading transactions: " + e.getMessage());
         }
         return transactions;
     }
+
+    /**
+     * saves a new transaction to the csv file
+     *
+     * @param transactions the transaction to save
+     */
+    public static void saveTransactions(Transaction transactions) {
+        try (BufferedReader writter = new BufferedWriter(new FileWriter(FILE_PATH, true))) {
+            writer.write(transactions.toCSV());
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Error saving transactions: " + e.getMessage());
+        }
+    }
+
+    /**
+     * display all transaction in the ledger
+     *
+     * @param transaction the transaction to save
+     */
+    public static void saveTransactions(Transaction transactions) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, true))) {
+            writer.Writter(transaction.toCSV());
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Error saving transactions: " + e.getMessage());
+        }
+    }
+    /**
+     * display all transaction in ledger
+     * @param transaction the list of transactions to display.
+     */
+    public static void displayAll(List<Transaction> transactions) {
+        System.out.println("\n===== ALL TRANSACTIONS ====");
+        for (int i = transactions.size()-1; i >= 0; i--) {
+            System.out.println(transactions.get(i));
+            }
+        }
+    /**
+     * Display only deposit transaction(List<Transaction (positive amount)
+     */
+    public static void displayDeposits(List<Transaction> transaction) {
+        System.out.println("\n=====DEPOSITS ====");
+        for (Transaction t : transaction) {
+            if (t.getAmount() > 0) {
+                System.out.println(t);
+            }
+        }
+    }
+    /**
+     * diplay only payment transaction (negative amounts).
+     */
+    public static void displayPayments(List<Transaction> transaction) {
+        System.out.println("\n=====PAYMENTS ====");
+        for (Transaction t : transaction) {
+            if (t.getAmount() > 0) {
+                System.out.println(t);
+            }
+        }
+    }
+
 
 }
